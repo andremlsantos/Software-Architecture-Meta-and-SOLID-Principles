f9f482517d5586ab946cfbbe38022ae10b2143ce
Andre Santos <a.santos@buddypayment.nl>
2021-06-09 15:18:21 +0200

    8 Refactoring to a Better Design

--- SRP/Refactored/PaymentModel.cs
+++ SRP/Refactored/PaymentModel.cs
@@ -1,7 +1,12 @@
 ﻿namespace SOLID.SRP.Refactored
 {
+    /*
+     * This is our abstraction way of buying a ticket
+     * The concrete classes, both extend the base class, to implement BuyTicket() method
+     */
     public abstract class PaymentModel
     {
+        // we need this details to be visible for the concrete implementations
         protected TicketDetails _ticketDetails;
 
         protected PaymentModel(TicketDetails ticketDetails)
